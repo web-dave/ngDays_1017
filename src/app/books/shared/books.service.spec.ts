d55da23b961fb9a2a3d72be257fbbe0652ece4a3
David Muellerchen <dave@webdave.de>
2017-10-14 17:40:13 +0200

    some comments to explain whats going on

--- src/app/books/shared/books.service.spec.ts
+++ src/app/books/shared/books.service.spec.ts
@@ -4,6 +4,8 @@ import { it } from '@angular/cli/lib/ast-tools/spec-utils';
 import { async, inject, TestBed } from '@angular/core/testing';
 
 import { BooksService } from './books.service';
+
+// Just mocks
 export const booksStub = [
   {
     'title': 'Design Patterns',
@@ -42,7 +44,6 @@ export const booksStub = [
     }
   }
 ]
-
 export const bookMock = {
   'title': 'Design Patterns123',
   'subtitle': 'Elements of Reusable Object-Oriented Software',
@@ -58,12 +59,14 @@ export const bookMock = {
 
 describe('BooksService', () => {
   beforeEach(() => {
+    // setup @ngModule for testing
     TestBed.configureTestingModule({
       providers: [BooksService],
       imports: [HttpClientModule, HttpClientTestingModule]
     });
   });
 
+  // check after each test there is no pending(open) request
   afterEach(inject([HttpTestingController], (backend: HttpTestingController) => {
     backend.verify();
   }))
@@ -74,10 +77,12 @@ describe('BooksService', () => {
 
   it('should return all books', inject([BooksService, HttpTestingController],
     (service: BooksService, backend: HttpTestingController) => {
+      // call service method and test IN the subscription. no need to use async anymore!!
       service.getBooks()
         .subscribe(books => {
           expect(books).toEqual(booksStub)
         });
+        // Wait for the call and response with mockdata  `.flush()`
         backend.expectOne('http://localhost:4730/books/').flush(booksStub, { status: 200, statusText: 'Ok' });
     }));
     
